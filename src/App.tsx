46ff14b262d12ab34fe6e57d043f17085ccd6b27
chanchanjung <kellycmj94@gmail.com>
2023-10-01 20:45:46 +0900

    Add interface.md

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {Main} from "./pages/Main";
+import {Interface} from "./pages/java/Interface";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Main/>}/>
+            <Route path="/interface" element={<Interface/>}/>
         </Routes>
     );
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,20 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row} from "../common/Container";
+import {Button} from "../common/Button";
+import {MarkdownRenderer} from "../common/MarkdownRenderer";
+import {useNavigate} from "react-router-dom";
 
 
 export function Main() {
+    const navigate = useNavigate();
     return (
-        <Container className="d-flex justify-content-center align-items-center" style={{height: "100vh"}}>
+        <Container style={{backgroundColor: "black", height: "100vh"}}>
             <Row className="d-flex justify-content-center align-items-center">
                 <Col/>
-                <Col md="auto" className="justify-content-center" style={{color: "white"}}>
-                    <Button variant="outline-light" className="rounded-5 mx-2">Algorithm</Button>
-                    <Button variant="outline-light" className="rounded-5">Java</Button>
+                <Col colSpan={3} style={{color: "white"}}>
+                    <div>
+                        <Button>Algorithm</Button>
+                        <Button onClick={() => navigate("/interface")} >Java</Button>
+                    </div>
                 </Col>
                 <Col/>
             </Row>
